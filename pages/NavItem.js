6ddb8ec9220d0a2065644e7dc6cce4f9d60386fc
Amith-Jagannath <amithjagannath02@gmail.com>
2023-06-17 22:33:28 +0530

    vercel config

--- pages/NavItem.js
+++ pages/NavItem.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 function NavItem({ text, href, active }) {
   return (
-    <Link legacyBehavior href={href}>
+    <Link legacyBehavior href="href">
       <a className={`nav__item ${active ? "active" : ""}`}>{text}</a>
     </Link>
   );
